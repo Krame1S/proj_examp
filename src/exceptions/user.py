d118dc94f7c7f38e6abe6eb46748f1d5691865eb
Ilya <iliaha2005@gmail.com>
2026-03-22 11:00:23 +0300

    feat(exceptions): add EmailAlreadyTaken exception

--- src/exceptions/user.py
+++ src/exceptions/user.py
@@ -7,3 +7,9 @@ class UserNotFound(AppException):
     status_code = status.HTTP_404_NOT_FOUND
     error_code = "USER_NOT_FOUND"
     default_message = "User not found"
+
+
+class EmailAlreadyTaken(AppException):
+    status_code = status.HTTP_409_CONFLICT
+    error_code = "USER_EMAIL_TAKEN"
+    default_message = "This email is already in use"
